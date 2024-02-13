6543bef276820243c31934e0ceba66ebfb030e11
Kashyap Kurmilla <124886947+kashyapkurmilla@users.noreply.github.com>
2024-02-13 10:42:44 +0530

    incomplete

---
+++ Lab-4/bubblesort.s
@@ -0,0 +1,48 @@
+;bubble Sort
+    AREA RESET,DATA,READONLY
+	EXPORT __Vectors
+__Vectors
+	DCD 0X10001000
+	DCD Reset_Handler
+	AREA mycode,CODE,READONLY
+	ENTRY
+	EXPORT Reset_Handler
+Reset_Handler
+	mov r4,#0
+	mov r1,#10		
+	ldr r0, =list
+	ldr r2, =result
+up 
+	ldr r3, [r0,r4]
+	str r3, [r2,r4]
+	add r4, #04
+	sub r1,#01
+	cmp r1,#00
+	bhi up
+	ldr r0, =result
+	; inner loop counter
+	mov r3, #10
+	sub r3, r3, #1
+	mov r9, r3 ; R9 contain no of passes
+	; outer loop counter
+outer_loop
+	mov r5, r0
+	mov r4, r3 ; R4 contains no of compare in a pass
+inner_loop
+	ldr r6, [r5], #4
+	ldr r7, [r5]
+	cmp r7, r6
+	; swap without swp instruction
+	strls r6, [r5]
+	strls r7, [r5, #-4]
+	subs r4, r4, #1
+	bne inner_loop
+	sub r3, #1
+	subs r9, r9, #1
+	bne outer_loop
+list dcd 0x10,0x05,0x33,0x24,0x56,0x77,0x21,0x04,0x87,0x01
+	AREA data1, data, readwrite
+result DCW 0
+	end
+STOP
+	B STOP
